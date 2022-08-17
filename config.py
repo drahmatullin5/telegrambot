e6b2b2bb44fdd7d8c2df7c59c674eda2b6ff367e
Dmitry <104850369+drahmatullin5@users.noreply.github.com>
2022-08-17 18:21:19 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = "5681716562:AAG6ikuwwAab6yUnFuceHlOEPg65P8k08Dc"
+
+keys = {
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'йена': 'JPY',
+    'фунт': 'GBP',
+    'биткоин' : 'BTC',
+    'эфириум' : 'ETH',
+    'тенге' : 'KZT',
+}
